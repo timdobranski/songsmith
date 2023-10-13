361e7c61b5e370d2a733e7f620594a440a667b92
Tim <timdobranski@gmail.com>
2023-10-13 16:11:55 -0700

    Completed temp home page

--- app/page.js
+++ app/page.js
@@ -5,7 +5,19 @@ import Logo from './components/Logo/Logo';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Logo />
+      <div className={styles.titleContainer}>
+        <Logo className={styles.logo}/>
+        <h1 className={`${styles.reflectedText} ${styles.title}`} data-text='SongSmith'>
+          SongSmith
+        </h1>
+        <Logo className={styles.logo}/>
+      </div>
+      <div className={styles.descriptionContainer}>
+        <h2 >UNLOCK YOUR SONGWRITING POTENTIAL</h2>
+        <p>A songwriting app built to teach music theory</p>
+        <p>Coming Winter 2023</p>
+      </div>
     </main>
+
   )
 }
