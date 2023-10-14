7c3cf528acde5fd3a79302da276c9748d4c69cfe
Tim <timdobranski@gmail.com>
2023-10-13 18:30:41 -0700

    Fixed Header import casing

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google';
-import Header from './components/Header/header';
+import Header from './components/Header/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
