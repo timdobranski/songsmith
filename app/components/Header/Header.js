9caa78b9c7e47b2dabee1af58a516c25ad3e2881
Tim <timdobranski@gmail.com>
2023-10-14 03:43:30 -0700

    Updated with background image

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,9 +1,11 @@
 import styles from './header.module.css'
+import Logo from '../Logo/Logo';
 
 export default function Header() {
   return (
     <div id={styles.headerContainer}>
-      <h1 id={styles.headerTitle}>Header Here</h1>
+      {/* <h1 id={styles.headerTitle}>Header Here</h1> */}
+      {/* <Logo /> */}
     </div>
   )
 }
